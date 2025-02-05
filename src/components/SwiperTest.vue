7055f4fab2d4466256d3687a0626c91118f3e509
DaniilIsupov <disupov1996@gmail.com>
2025-02-05 17:40:30 +0200

    first commit

---
+++ src/components/SwiperTest.vue
@@ -0,0 +1,25 @@
+<template>
+    <Swiper
+        :loop="true"
+        @swiper="console.log('onSwiper', $event)"
+    >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        ...
+    </Swiper>
+</template>
+<script lang="ts" setup>
+import { onMounted, onUnmounted } from 'vue'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+
+import 'swiper/css'
+
+onMounted(() => {
+    console.log('onMounted')
+})
+
+onUnmounted(() => {
+    console.log('onUnmounted')
+})
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref, h } from 'vue'
+
+import SwiperTest from '../components/SwiperTest.vue'
+
+const swiper = ref(true)
+
+const vnode = h('div', ['test'])
+</script>
+
+<template>
+    <main>
+        <component
+            :is="swiper ? SwiperTest : vnode"
+            @vue:before-mount="swiper = false"
+            @vue:mounted="console.log('mounted', $event)"
+            @vue:unmounted="console.log('unmounted', $event)"
+        ></component>
+    </main>
+</template>
